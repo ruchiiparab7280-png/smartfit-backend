0647ea747cba20744c87162fe4ff8fd42d1c982e
Ruchita <ruchiiparab7280@gmail.com>
2026-02-27 22:13:02 +0530

    frontend connected to live backend

--- server.js
+++ server.js
@@ -269,6 +269,5 @@ app.post("/contact", async (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log(`Server running on port ${PORT}`);
 });
-
